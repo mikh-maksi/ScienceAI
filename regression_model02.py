f759580866534f73ba1ee6c0a5bf9fe26204b447
mikh-maksi <newlevelcenter@gmail.com>
2025-07-09 16:42:30 +0300

    regression

---
+++ regression_model02.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#polynomial fit with degree = 3
+model = np.poly1d(np.polyfit(x, y,3))
+
+#add fitted polynomial line to scatterplot
+polyline = np.linspace(2, 13, 13)
+plt.scatter(x, y)
+plt.plot(polyline, model(polyline))
+plt.show()
